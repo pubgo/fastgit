065b78cd072c0dd8ccaf7cb82355b8b014a76217
barry <kookshine@gmail.com>
2026-03-03 17:07:09 +0800

    chore: quick update feat/ssh_session at 2026-03-03 17:07:08

--- bootstrap/boot.go
+++ bootstrap/boot.go
@@ -16,6 +16,7 @@ import (
 	"github.com/pubgo/fastgit/cmds/historycmd"
 	"github.com/pubgo/fastgit/cmds/initcmd"
 	"github.com/pubgo/fastgit/cmds/pullcmd"
+	"github.com/pubgo/fastgit/cmds/sshcmd"
 	"github.com/pubgo/fastgit/cmds/tagcmd"
 	"github.com/pubgo/fastgit/cmds/upgradecmd"
 	"github.com/pubgo/fastgit/cmds/versioncmd"
@@ -35,6 +36,7 @@ func Main() {
 		initcmd.New(),
 		upgradecmd.New(),
 		tagcmd.New(),
+		sshcmd.New(),
 		historycmd.New(),
 		fastcommitcmd.New(),
 		configcmd.New(),
